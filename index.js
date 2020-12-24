afd74a21fe211837c78f2a78fc054c387e829b03
UUIEEE <76521515+UUIEEE@users.noreply.github.com>
2020-12-24 11:56:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-
-
 dc=document;
 bdy=()=>{gb()}
 gto=a=>{window.open(a,'_blank')}
-
-function gb(a="pmt")
-{
+gb=(a="pmt")=>{
 	let r=dc.getElementById(a).style.display;
 	dc.getElementById(a).style.display=r=="none"?"block":"none";
 }
